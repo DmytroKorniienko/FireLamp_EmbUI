9ead1ac5395549ca1dd9a41378dbc9186f6da2ec
Emil Muratov <gpm@hotplug.ru>
2021-02-04 11:05:54 +0300

    Fix replace_fs.py to work with under windows
    Signed-off-by: Emil Muratov <gpm@hotplug.ru>

--- replace_fs.py
+++ replace_fs.py
@@ -1,3 +1,9 @@
 Import("env")
-print("Replace MKSPIFFSTOOL with mklittlefs")
-env.Replace (MKSPIFFSTOOL = "./mklittlefs")
+import platform
+
+if platform.system()  == 'Windows':
+    print("Replace MKSPIFFSTOOL with mklittlefs.exe")
+    env.Replace (MKSPIFFSTOOL = "./mklittlefs.exe")
+else:
+    print("Replace MKSPIFFSTOOL with mklittlefs")
+    env.Replace (MKSPIFFSTOOL = "./mklittlefs")
