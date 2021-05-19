899712719d673aabec391345ce32232b01c4ee76
Dmytro Korniienko <Dmytro.Korniienko@volo.global>
2021-05-19 18:49:30 +0300

    resources fix

--- resources/respack.cmd
+++ resources/respack.cmd
@@ -1,3 +1,4 @@
+@rem Look at this for zip command support: https://stackoverflow.com/questions/38782928/how-to-add-man-and-zip-to-git-bash-installation-on-windows
 setlocal
 set workdir=%~dp0
 PATH=%PATH%;%workdir%;%ProgramFiles%\Git
