5280188efec9505a7d2510374aae9b88367ad9aa
kostyamat <kostyamat@yahoo.com>
2021-07-19 20:45:31 +0200

    Правка прокрутки в мониторе порта

--- SerialMonitor.cmd
+++ SerialMonitor.cmd
@@ -8,6 +8,5 @@ if exist "%USERPROFILE%\Python\python.exe" (
 )
 PATH=%PATH%;%workdir%;%USERPROFILE%\.platformio\penv\Scripts;%PYTHONPATH%;
 @chcp UTF8>nul
-mode con: cols=88 lines=40
 cls 
 platformio.exe device monitor
